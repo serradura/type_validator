ec7d535d139f263a23cf3e2885b3a98935197205
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2019-09-18 01:40:18 -0300

    Turn TypeValidator#fetch_strategy into a private method

--- lib/type_validator.rb
+++ lib/type_validator.rb
@@ -16,13 +16,15 @@ class TypeValidator < ActiveModel::EachValidator
     record.errors.add(attribute, error)
   end
 
-  def fetch_strategy(options)
-    return ByKindOf if options.key?(:is_a) || options.key?(:kind_of)
-    return ByRespondTo if options.key?(:respond_to)
-    return ByKlass if options.key?(:klass)
-    return ByArrayOf if options.key?(:array_of)
-    return ByArrayWith if options.key?(:array_with)
-  end
+  private
+
+    def fetch_strategy(options)
+      return ByKindOf if options.key?(:is_a) || options.key?(:kind_of)
+      return ByRespondTo if options.key?(:respond_to)
+      return ByKlass if options.key?(:klass)
+      return ByArrayOf if options.key?(:array_of)
+      return ByArrayWith if options.key?(:array_with)
+    end
 end
 
 require 'type_validator/version'
