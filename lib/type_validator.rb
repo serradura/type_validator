54c0a3c5051696e01e400979d8c533751bc2fe80
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2019-09-18 22:13:16 -0300

    Optimize the validations

--- lib/type_validator.rb
+++ lib/type_validator.rb
@@ -4,12 +4,9 @@ require 'active_model'
 
 class TypeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    strategy = fetch_strategy(options)
-
-    raise Error::InvalidDefinition.new(attribute) unless strategy
-
     return if options[:allow_nil] && value.nil?
-    return unless error = strategy.invalid?(value, options)
+
+    return unless error = validate_type_of(attribute, value)
 
     raise TypeError, "#{attribute} #{error}" if options[:strict]
 
@@ -18,19 +15,60 @@ class TypeValidator < ActiveModel::EachValidator
 
   private
 
-    def fetch_strategy(options)
-      return ByKindOf if options.key?(:is_a) || options.key?(:kind_of)
-      return ByRespondTo if options.key?(:respond_to)
-      return ByKlass if options.key?(:klass)
-      return ByArrayOf if options.key?(:array_of)
-      return ByArrayWith if options.key?(:array_with)
+    def validate_type_of(attribute, value)
+      if expected = options[:is_a]      ; return validate_kind_of(value, expected)   ; end
+      if expected = options[:kind_of]   ; return validate_kind_of(value, expected)   ; end
+      if expected = options[:respond_to]; return validate_respond_to(value, expected); end
+      if expected = options[:klass]     ; return validate_klass(value, expected)     ; end
+      if expected = options[:array_of]  ; return validate_array_of(value, expected)  ; end
+      if expected = options[:array_with]; return validate_array_with(value, expected); end
+
+      raise Error::InvalidDefinition.new(attribute)
+    end
+
+    def validate_kind_of(value, expected)
+      types = Array(expected)
+
+      return if types.any? { |type| value.is_a?(type) }
+
+      "must be a kind of: #{types.map { |klass| klass.name }.join(', ')}"
+    end
+
+    def validate_respond_to(value, method_name)
+      return if value.respond_to?(method_name)
+
+      "must respond to the method `#{method_name}`"
+    end
+
+    def validate_klass(value, klass)
+      require_a_class(value)
+      require_a_class(klass)
+
+      return if value == klass || value < klass
+
+      "must be the or a subclass of `#{klass.name}`"
+    end
+
+    def require_a_class(arg)
+      raise ArgumentError, "#{arg} must be a class" unless arg.is_a?(Class)
+    end
+
+    def validate_array_of(value, expected)
+      types = Array(expected)
+
+      return if value.is_a?(Array) && !value.empty? && value.all? { |value| types.any? { |type| value.is_a?(type) } }
+
+      "must be an array of: #{types.map { |klass| klass.name }.join(', ')}"
+    end
+
+    def validate_array_with(value, expected)
+      raise ArgumentError, "#{expected} must be an array" unless expected.is_a?(Array)
+
+      return if value.is_a?(Array) && !value.empty? && (value - expected).empty?
+
+      "must be an array with: #{expected.join(', ')}"
     end
 end
 
 require 'type_validator/version'
 require 'type_validator/error'
-require 'type_validator/by_klass'
-require 'type_validator/by_kind_of'
-require 'type_validator/by_array_of'
-require 'type_validator/by_array_with'
-require 'type_validator/by_respond_to'
